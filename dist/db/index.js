fc5c545313d1f44a7e2027afc94166f32ff3aca9
SaimManzoor49 <118682379+SaimManzoor49@users.noreply.github.com>
2025-03-08 21:46:24 +0500

    Update index.js

--- dist/db/index.js
+++ dist/db/index.js
@@ -7,7 +7,10 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const index_1 = require("../constants/index");
 const connectToDB = async () => {
     try {
-        const conn = await mongoose_1.default.connect(`mongodb+srv://saimmanzoor:Password@cluster0.tkv8u4s.mongodb.net/aitools`);
+        const conn = await mongoose_1.default.connect(
+            `mongodb://saimmanzoor:Password@ac-qxhhdng-shard-00-00.tkv8u4s.mongodb.net:27017,ac-qxhhdng-shard-00-01.tkv8u4s.mongodb.net:27017,ac-qxhhdng-shard-00-02.tkv8u4s.mongodb.net:27017/aitools?ssl=true&replicaSet=atlas-q74vx9-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`,
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        );
         console.log(`\n MongoDB connected to Host: ${conn.connection.host}`);
     }
     catch (error) {
